6908e5767ea840e6a6833eee777f57871f2160cd
kefearr <kefears.post@gmail.com>
2023-04-19 18:27:17 +0300

    try different colour format (hsv) switch only, not working

--- sort_service/source.py
+++ sort_service/source.py
@@ -1,5 +1,13 @@
+#####task####task####task####task####task####task####task####task####task####task####task#######
+# need to add gammacorr+ with any condition to lowgamma image
+# or
+# try HSV
+# or
+# need another way to solve this shit
+################################################################################################
 # python 3.10 
 # cv2
+################################################################################################
 import cv2
 import os
 
@@ -14,24 +22,50 @@ def fill_array(array: dict):
             array[str(filename)] = img
     return array
 
+# RGB -> resize -> hsv
+def format_hsv_image(image):
+    # one size resize
+    width = 1
+    height = 1
+
+    # percent resize
+    # width = int(array[image].shape[1] * scale_percent / 100)
+    # height = int(array[image].shape[0] * scale_percent / 100)
+
+    dim = (width, height)
+    img = image.copy() #copy for resizing
+    img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    return img
+
+# RGB resize
+def resize_image(image):
+    # one size resize
+    width = 1
+    height = 1
+
+    # percent resize
+    # scale_percent = 2
+    # width = int(array[image].shape[1] * scale_percent / 100)
+    # height = int(array[image].shape[0] * scale_percent / 100)
+
+    dim = (width, height)
+    img = image.copy() #copy for resizing
+    img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    return img
+
 # try to calculate average image's colour 
 def mean_colour(array: dict):
     img_list = {}
-    scale_percent = 5 # percent of original size for percent resize
-    # print(array.keys())
+
+    # resize/format
     for image in array.keys():
 
-        # one size resize
-        width = 4
-        height = 4
+        # this one for RGB (BGR actualy) 
+        # img_list[image] = resize_image(array[image])
 
-        # percent resize
-        # width = int(array[image].shape[1] * scale_percent / 100)
-        # height = int(array[image].shape[0] * scale_percent / 100)
-        dim = (width, height)
-        img = array[image].copy() #copy for resizing
-        img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-        img_list[image] = img
+        # this one for HSV
+        img_list[image] = format_hsv_image(array[image])
     
     average_value_colour = {}
     for img in img_list.keys():
@@ -43,20 +77,24 @@ def mean_colour(array: dict):
 
         for i in range(width): #Цикл по ширине
             for j in range(height): #Цикл по высоте
-                sq[0] += img_list[img][i, j][0] #b
-                sq[1] += img_list[img][i, j][1] #g
-                sq[2] += img_list[img][i, j][2] #r
+                sq[0] += img_list[img][i, j][0] #b or h (hue)
+                sq[1] += img_list[img][i, j][1] #g or s (saturation)
+                sq[2] += img_list[img][i, j][2] #r or v (value)
 	
         out = [0, 0, 0] #Массив для средних значений
         out[0] = int(sq[0]/count) #Средние значения
         out[1] = int(sq[1]/count)
         out[2] = int(sq[2]/count)
         average_value_colour[img] = out
+
+        # print for rgb
         # print(f'Средний цвет: rgb({out[2]}, {out[1]}, {out[0]})')
         # hexed = '#' + format(out[2], 'x') + format(out[1], 'x') + format(out[0], 'x') #Перевод в HEX
         # print(f'hex: {hexed}')
+
     return average_value_colour
     
+# try to processing palette
 def temp_processing(average_arr: dict, target_colour: str):
     result = []
 
@@ -94,10 +132,24 @@ def temp_processing(average_arr: dict, target_colour: str):
                     average_arr[filename][1] > average_arr[filename][2]:
                     result.append(filename)
 
+        case 'yellow':
+            for filename in average_arr.keys():
+                if average_arr[filename][2] == max(average_arr[filename]) and \
+                    average_arr[filename][1] in range(average_arr[filename][0]+20, average_arr[filename][2]):
+                    result.append(filename)
+
         case 'red':
             for filename in average_arr.keys():
-                if average_arr[filename][2] > average_arr[filename][1] and \
-                    average_arr[filename][2] > average_arr[filename][0]:
+                if average_arr[filename][2] == max(average_arr[filename]) and \
+                    average_arr[filename][2]-10 > (average_arr[filename][0]+average_arr[filename][0])/2:
+                    result.append(filename)
+        case 'pink':
+            for filename in average_arr.keys():
+                # if average_arr[filename][2] >= 190 and \
+                if max(average_arr[filename]) == average_arr[filename][2] and \
+                average_arr[filename][1] == min(average_arr[filename]) and \
+                average_arr[filename][1] < 200 and \
+                average_arr[filename][0] in range(average_arr[filename][1], average_arr[filename][2]-20):
                     result.append(filename)
 
         case _:
@@ -105,10 +157,15 @@ def temp_processing(average_arr: dict, target_colour: str):
 
     return result
 
+# try to processing palette in hsv colours
+def hsv_processing(average_arr: dict, target_colour:str):
+    pass
+
+
 if __name__ == '__main__':
     dick = {}
     dick = fill_array(dick)
     average_colour_value_dick = mean_colour(dick)
     print('\n'.join("{}\t{}".format(k, v) for k, v in average_colour_value_dick.items()))
-    result = temp_processing(average_arr=average_colour_value_dick, target_colour = 'yellow')
-    print(result)
+    # result = temp_processing(average_arr=average_colour_value_dick, target_colour = 'red')
+    # print(result)
