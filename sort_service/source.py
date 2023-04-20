d16146c710faf5ab25048fa14d2f09ab9a3b3c85
kefearr <kefears.post@gmail.com>
2023-04-20 15:54:08 +0300

    starting hsv processing, looking good actually

--- sort_service/source.py
+++ sort_service/source.py
@@ -94,72 +94,146 @@ def mean_colour(array: dict):
 
     return average_value_colour
     
-# try to processing palette
-def temp_processing(average_arr: dict, target_colour: str):
+# try to processing palette rgb
+# def temp_processing(average_arr: dict, target_colour: str):
+#     result = []
+
+#     #conditions needs to be changed
+#     match target_colour:
+#         case 'black':
+#             for filename in average_arr.keys():
+#                 acceptable_black_or_white_range = range(min(average_arr[filename]), min(average_arr[filename])+10, 1)
+
+#                 if average_arr[filename][0] in acceptable_black_or_white_range and \
+#                     average_arr[filename][1] in acceptable_black_or_white_range and \
+#                         average_arr[filename][1] in acceptable_black_or_white_range and \
+#                             min(acceptable_black_or_white_range) <= 120:
+#                     result.append(filename)
+
+#         case 'white':
+#             for filename in average_arr.keys():
+#                 acceptable_black_or_white_range = range(min(average_arr[filename]), min(average_arr[filename])+10, 1)
+
+#                 if average_arr[filename][0] in acceptable_black_or_white_range and \
+#                     average_arr[filename][1] in acceptable_black_or_white_range and \
+#                         average_arr[filename][1] in acceptable_black_or_white_range and \
+#                             min(acceptable_black_or_white_range) >= 120:
+#                     result.append(filename)
+
+#         case 'blue':
+#             for filename in average_arr.keys():
+#                 if average_arr[filename][0] > average_arr[filename][1] and \
+#                     average_arr[filename][0] > average_arr[filename][2]:
+#                     result.append(filename)
+
+#         case 'green':
+#             for filename in average_arr.keys():
+#                 if average_arr[filename][1] > average_arr[filename][0] and \
+#                     average_arr[filename][1] > average_arr[filename][2]:
+#                     result.append(filename)
+
+#         case 'yellow':
+#             for filename in average_arr.keys():
+#                 if average_arr[filename][2] == max(average_arr[filename]) and \
+#                     average_arr[filename][1] in range(average_arr[filename][0]+20, average_arr[filename][2]):
+#                     result.append(filename)
+
+#         case 'red':
+#             for filename in average_arr.keys():
+#                 if average_arr[filename][2] == max(average_arr[filename]) and \
+#                     average_arr[filename][2]-10 > (average_arr[filename][0]+average_arr[filename][0])/2:
+#                     result.append(filename)
+#         case 'pink':
+#             for filename in average_arr.keys():
+#                 # if average_arr[filename][2] >= 190 and \
+#                 if max(average_arr[filename]) == average_arr[filename][2] and \
+#                 average_arr[filename][1] == min(average_arr[filename]) and \
+#                 average_arr[filename][1] < 200 and \
+#                 average_arr[filename][0] in range(average_arr[filename][1], average_arr[filename][2]-20):
+#                     result.append(filename)
+
+#         case _:
+#             print('oh hellow there, your colour is not supported')
+
+#     return result
+
+# try to processing palette in hsv colours
+def hsv_processing(average_arr: dict, target_colour:str):
+    # red (h = 0 +- 10) (s = 255) (v = 255)
+    # yellow (h = 30 +- 10) (s = 255) (v = 255)
+    # lime (h = 60 +- 10) (s = 255) (v = 255)
+    # teal (h = 90 +- 10) (s = 255) (v = 128)
+    # blue (h = 120 +- 10) (s = 255) (v = 255)
+    # violet (h = 150 +- 10) (s = 116) (v = 238)
+    # purple (h = 150 +- 10) (s = 255) (v = 128)
+
+    # black (h = 0 +- 10) (s = 0) (v = 0)
+    # gray 0 0 128
+    # white 0 0 255
+    color_table = {
+        'black': [range(0, 255), range(0, 30), range(0,50)],
+        'gray': [range(0, 255), range(0, 30), range(51,189)],
+        'white': [range(0, 255), range(0, 30), range(190,255)],
+        'red': [range(0, 10), range(70, 255), range(40,255)],
+        'red2': [range(245, 255), range(70, 255), range(40,255)],
+        'orange': [range(11, 25), range(60, 255), range(40,170)],
+        'yellow': [range(20, 40), range(140, 255), range(100,255)],
+        'green': [range(41, 80), range(50, 255), range(50,255)]
+    }
     result = []
 
-    #conditions needs to be changed
     match target_colour:
         case 'black':
             for filename in average_arr.keys():
-                acceptable_black_or_white_range = range(min(average_arr[filename]), min(average_arr[filename])+10, 1)
-
-                if average_arr[filename][0] in acceptable_black_or_white_range and \
-                    average_arr[filename][1] in acceptable_black_or_white_range and \
-                        average_arr[filename][1] in acceptable_black_or_white_range and \
-                            min(acceptable_black_or_white_range) <= 120:
+                if average_arr[filename][1] in color_table['black'][1] and \
+                average_arr[filename][2] in color_table['black'][2]:
                     result.append(filename)
-
+        case 'gray':
+            for filename in average_arr.keys():
+                if average_arr[filename][1] in color_table['grey'][1] and \
+                average_arr[filename][2] in color_table['grey'][2]:
+                    result.append(filename)
         case 'white':
             for filename in average_arr.keys():
-                acceptable_black_or_white_range = range(min(average_arr[filename]), min(average_arr[filename])+10, 1)
-
-                if average_arr[filename][0] in acceptable_black_or_white_range and \
-                    average_arr[filename][1] in acceptable_black_or_white_range and \
-                        average_arr[filename][1] in acceptable_black_or_white_range and \
-                            min(acceptable_black_or_white_range) >= 120:
-                    result.append(filename)
-
-        case 'blue':
-            for filename in average_arr.keys():
-                if average_arr[filename][0] > average_arr[filename][1] and \
-                    average_arr[filename][0] > average_arr[filename][2]:
-                    result.append(filename)
-
-        case 'green':
-            for filename in average_arr.keys():
-                if average_arr[filename][1] > average_arr[filename][0] and \
-                    average_arr[filename][1] > average_arr[filename][2]:
-                    result.append(filename)
-
-        case 'yellow':
-            for filename in average_arr.keys():
-                if average_arr[filename][2] == max(average_arr[filename]) and \
-                    average_arr[filename][1] in range(average_arr[filename][0]+20, average_arr[filename][2]):
+                if average_arr[filename][1] in color_table['white'][1] and \
+                average_arr[filename][2] in color_table['white'][2]:
                     result.append(filename)
 
         case 'red':
             for filename in average_arr.keys():
-                if average_arr[filename][2] == max(average_arr[filename]) and \
-                    average_arr[filename][2]-10 > (average_arr[filename][0]+average_arr[filename][0])/2:
+                if (average_arr[filename][0] in color_table['red'][0] and \
+                    average_arr[filename][1] in color_table['red'][1] and \
+                    average_arr[filename][2] in color_table['red'][2]) or \
+                    (average_arr[filename][0] in color_table['red'][0] and \
+                    average_arr[filename][1] in color_table['red2'][1] and \
+                    average_arr[filename][2] in color_table['red2'][2]):
                     result.append(filename)
-        case 'pink':
+        case 'orange':
             for filename in average_arr.keys():
-                # if average_arr[filename][2] >= 190 and \
-                if max(average_arr[filename]) == average_arr[filename][2] and \
-                average_arr[filename][1] == min(average_arr[filename]) and \
-                average_arr[filename][1] < 200 and \
-                average_arr[filename][0] in range(average_arr[filename][1], average_arr[filename][2]-20):
+                if average_arr[filename][0] in color_table['orange'][0] and \
+                    average_arr[filename][1] in color_table['orange'][1] and \
+                    average_arr[filename][2] in color_table['orange'][2]:
                     result.append(filename)
-
+        case 'yellow':
+            for filename in average_arr.keys():
+                if average_arr[filename][0] in color_table['yellow'][0] and \
+                    average_arr[filename][1] in color_table['yellow'][1] and \
+                    average_arr[filename][2] in color_table['yellow'][2]:
+                    result.append(filename)
+        case 'green':
+            for filename in average_arr.keys():
+                if average_arr[filename][0] in color_table['green'][0] and \
+                    average_arr[filename][1] in color_table['green'][1] and \
+                    average_arr[filename][2] in color_table['green'][2]:
+                    result.append(filename)
+    
         case _:
             print('oh hellow there, your colour is not supported')
-
     return result
 
-# try to processing palette in hsv colours
-def hsv_processing(average_arr: dict, target_colour:str):
-    pass
+
+
+
 
 
 if __name__ == '__main__':
@@ -167,5 +241,5 @@ if __name__ == '__main__':
     dick = fill_array(dick)
     average_colour_value_dick = mean_colour(dick)
     print('\n'.join("{}\t{}".format(k, v) for k, v in average_colour_value_dick.items()))
-    # result = temp_processing(average_arr=average_colour_value_dick, target_colour = 'red')
-    # print(result)
+    result = hsv_processing(average_arr=average_colour_value_dick, target_colour = 'green')
+    print(result)
