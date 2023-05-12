7d4624fdf8e53afabb05e3f34b321dfff03a5e86
kefearr <kefears.post@gmail.com>
2023-05-12 20:46:01 +0300

    now user must choose img as target like colour, and threshold

--- sort_service/source.py
+++ sort_service/source.py
@@ -7,7 +7,7 @@ import os
 
 # filling array 
 # with upload images
-def fill_array(array: dict):
+def fill_array(array: dict) -> dict:
     for filename in os.listdir(os.path.join(os.path.dirname(__file__),'array')):
         if filename[filename.rfind(".") + 1:] in ['jpg', 'jpeg', 'png']:
             # print(filename)
@@ -48,8 +48,28 @@ def resize_image(image):
     img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
     return img
 
+
+def calculate_mean_colour(image):
+    size = image.shape
+    sq = [0,0,0] #Массив для общего подсчета
+    width = size[0] #Ширина
+    height = size[1] #Высота
+    count = width*height #Ширина * Высота
+
+    for i in range(width): #Цикл по ширине
+        for j in range(height): #Цикл по высоте
+            sq[0] += image[i, j][0] #b or h (hue)
+            sq[1] += image[i, j][1] #g or s (saturation)
+            sq[2] += image[i, j][2] #r or v (value)
+
+    out = [0, 0, 0] #Массив для средних значений
+    out[0] = int(sq[0]/count) #Средние значения
+    out[1] = int(sq[1]/count)
+    out[2] = int(sq[2]/count)
+    return out
+
 # try to calculate average image's colour 
-def mean_colour(array: dict):
+def mean_colour(array: dict) -> dict:
     img_list = {}
 
     # resize/format
@@ -63,26 +83,9 @@ def mean_colour(array: dict):
     
     average_value_colour = {}
     for img in img_list.keys():
-        size = img_list[img].shape
-        sq = [0,0,0] #Массив для общего подсчета
-        width = size[0] #Ширина
-        height = size[1] #Высота
-        count = width*height #Ширина * Высота
-
-        for i in range(width): #Цикл по ширине
-            for j in range(height): #Цикл по высоте
-                sq[0] += img_list[img][i, j][0] #b or h (hue)
-                sq[1] += img_list[img][i, j][1] #g or s (saturation)
-                sq[2] += img_list[img][i, j][2] #r or v (value)
-	
-        out = [0, 0, 0] #Массив для средних значений
-        out[0] = int(sq[0]/count) #Средние значения
-        out[1] = int(sq[1]/count)
-        out[2] = int(sq[2]/count)
-        average_value_colour[img] = out
-
+        average_value_colour[img] = calculate_mean_colour(img_list[img])
         # print for rgb
-        # print(f'Средний цвет: rgb({out[2]}, {out[1]}, {out[0]})')
+        # print(f'Средний цвет: {img}({average_value_colour[img][0]}, {average_value_colour[img][1]}, {average_value_colour[img][2]})')
         # hexed = '#' + format(out[2], 'x') + format(out[1], 'x') + format(out[0], 'x') #Перевод в HEX
         # print(f'hex: {hexed}')
 
@@ -152,108 +155,143 @@ def mean_colour(array: dict):
 #     return result
 
 # try to processing palette in hsv colours
-def hsv_processing(average_arr: dict, target_colour:str) -> list:
-    # red (h = 0 +- 10) (s = 255) (v = 255)
-    # yellow (h = 30 +- 10) (s = 255) (v = 255)
-    # lime (h = 60 +- 10) (s = 255) (v = 255)
-    # teal (h = 90 +- 10) (s = 255) (v = 128)
-    # blue (h = 120 +- 10) (s = 255) (v = 255)
-    # violet (h = 150 +- 10) (s = 116) (v = 238)
-    # purple (h = 150 +- 10) (s = 255) (v = 128)
+# def hsv_processing(average_arr: dict, target_colour:str) -> list:
+#     # red (h = 0 +- 10) (s = 255) (v = 255)
+#     # yellow (h = 30 +- 10) (s = 255) (v = 255)
+#     # lime (h = 60 +- 10) (s = 255) (v = 255)
+#     # teal (h = 90 +- 10) (s = 255) (v = 128)
+#     # blue (h = 120 +- 10) (s = 255) (v = 255)
+#     # violet (h = 150 +- 10) (s = 116) (v = 238)
+#     # purple (h = 150 +- 10) (s = 255) (v = 128)
 
-    # black (h = 0 +- 10) (s = 0) (v = 0)
-    # gray 0 0 128
-    # white 0 0 255
-    color_table = {
-        'black': [range(0, 179), range(0, 30), range(0,50)],
-        'gray': [range(0, 179), range(0, 30), range(51,189)],
-        'white': [range(0, 179), range(0, 30), range(190,255)],
-        'red': [range(0, 10), range(70, 255), range(40,255)],
-        'red2': [range(161, 179), range(70, 255), range(40,255)],
-        'orange': [range(11, 25), range(60, 255), range(40,170)],
-        'yellow': [range(20, 40), range(140, 255), range(100,255)],
-        'green': [range(41, 79), range(50, 255), range(50,255)],
-        'teal': [range(80, 100), range(50, 255), range(50,255)],
-        'blue': [range(101, 139), range(50, 255), range(50,255)],
-        'purple': [range(140, 160), range(50, 255), range(50,255)]
-    }
+#     # black (h = 0 +- 10) (s = 0) (v = 0)
+#     # gray 0 0 128
+#     # white 0 0 255
+#     color_table = {
+#         'black': [range(0, 179), range(0, 30), range(0,50)],
+#         'gray': [range(0, 179), range(0, 30), range(51,189)],
+#         'white': [range(0, 179), range(0, 30), range(190,255)],
+#         'red': [range(0, 10), range(70, 255), range(40,255)],
+#         'red2': [range(161, 179), range(70, 255), range(40,255)],
+#         'orange': [range(11, 25), range(60, 255), range(40,170)],
+#         'yellow': [range(20, 40), range(140, 255), range(100,255)],
+#         'green': [range(41, 79), range(50, 255), range(50,255)],
+#         'teal': [range(80, 100), range(50, 255), range(50,255)],
+#         'blue': [range(101, 139), range(50, 255), range(50,255)],
+#         'purple': [range(140, 160), range(50, 255), range(50,255)]
+#     }
+#     result = []
+
+#     match target_colour:
+#         case 'black':
+#             for filename in average_arr.keys():
+#                 if average_arr[filename][1] in color_table['black'][1] and \
+#                 average_arr[filename][2] in color_table['black'][2]:
+#                     result.append(filename)
+#         case 'gray':
+#             for filename in average_arr.keys():
+#                 if average_arr[filename][1] in color_table['grey'][1] and \
+#                 average_arr[filename][2] in color_table['grey'][2]:
+#                     result.append(filename)
+#         case 'white':
+#             for filename in average_arr.keys():
+#                 if average_arr[filename][1] in color_table['white'][1] and \
+#                 average_arr[filename][2] in color_table['white'][2]:
+#                     result.append(filename)
+#         case 'red':
+#             for filename in average_arr.keys():
+#                 if (average_arr[filename][0] in color_table['red'][0] and \
+#                     average_arr[filename][1] in color_table['red'][1] and \
+#                     average_arr[filename][2] in color_table['red'][2]) or \
+#                     (average_arr[filename][0] in color_table['red'][0] and \
+#                     average_arr[filename][1] in color_table['red2'][1] and \
+#                     average_arr[filename][2] in color_table['red2'][2]):
+#                     result.append(filename)
+#         case 'orange':
+#             for filename in average_arr.keys():
+#                 if average_arr[filename][0] in color_table['orange'][0] and \
+#                     average_arr[filename][1] in color_table['orange'][1] and \
+#                     average_arr[filename][2] in color_table['orange'][2]:
+#                     result.append(filename)
+#         case 'yellow':
+#             for filename in average_arr.keys():
+#                 if average_arr[filename][0] in color_table['yellow'][0] and \
+#                     average_arr[filename][1] in color_table['yellow'][1] and \
+#                     average_arr[filename][2] in color_table['yellow'][2]:
+#                     result.append(filename)
+#         case 'green':
+#             for filename in average_arr.keys():
+#                 if average_arr[filename][0] in color_table['green'][0] and \
+#                     average_arr[filename][1] in color_table['green'][1] and \
+#                     average_arr[filename][2] in color_table['green'][2]:
+#                     result.append(filename)
+#         case 'teal':
+#             for filename in average_arr.keys():
+#                 if average_arr[filename][0] in color_table['teal'][0] and \
+#                     average_arr[filename][1] in color_table['teal'][1] and \
+#                     average_arr[filename][2] in color_table['teal'][2]:
+#                     result.append(filename)
+#         case 'blue':
+#             for filename in average_arr.keys():
+#                 if average_arr[filename][0] in color_table['blue'][0] and \
+#                     average_arr[filename][1] in color_table['blue'][1] and \
+#                     average_arr[filename][2] in color_table['blue'][2]:
+#                     result.append(filename)
+#         case 'purple':
+#             for filename in average_arr.keys():
+#                 if average_arr[filename][0] in color_table['purple'][0] and \
+#                     average_arr[filename][1] in color_table['purple'][1] and \
+#                     average_arr[filename][2] in color_table['purple'][2]:
+#                     result.append(filename)
+#         case _:
+#             print("oh hellow there! \n the program was created by a guy, who doesn't know this color name, try something simpler")
+#             return
+#     return result
+
+# def do_this(target:str) -> list:
+#     dick = {}
+#     dick = fill_array(dick)
+#     average_colour_value_dick = mean_colour(dick)
+#     # print('\n'.join("{}\t{}".format(k, v) for k, v in average_colour_value_dick.items()))
+#     result = hsv_processing(average_arr=average_colour_value_dick, target_colour = target)
+#     return result
+
+def hsv_processing_harder(average_arr: dict, target_colour: list, threshold: list) -> list:
     result = []
-
-    match target_colour:
-        case 'black':
-            for filename in average_arr.keys():
-                if average_arr[filename][1] in color_table['black'][1] and \
-                average_arr[filename][2] in color_table['black'][2]:
-                    result.append(filename)
-        case 'gray':
-            for filename in average_arr.keys():
-                if average_arr[filename][1] in color_table['grey'][1] and \
-                average_arr[filename][2] in color_table['grey'][2]:
-                    result.append(filename)
-        case 'white':
-            for filename in average_arr.keys():
-                if average_arr[filename][1] in color_table['white'][1] and \
-                average_arr[filename][2] in color_table['white'][2]:
-                    result.append(filename)
-        case 'red':
-            for filename in average_arr.keys():
-                if (average_arr[filename][0] in color_table['red'][0] and \
-                    average_arr[filename][1] in color_table['red'][1] and \
-                    average_arr[filename][2] in color_table['red'][2]) or \
-                    (average_arr[filename][0] in color_table['red'][0] and \
-                    average_arr[filename][1] in color_table['red2'][1] and \
-                    average_arr[filename][2] in color_table['red2'][2]):
-                    result.append(filename)
-        case 'orange':
-            for filename in average_arr.keys():
-                if average_arr[filename][0] in color_table['orange'][0] and \
-                    average_arr[filename][1] in color_table['orange'][1] and \
-                    average_arr[filename][2] in color_table['orange'][2]:
-                    result.append(filename)
-        case 'yellow':
-            for filename in average_arr.keys():
-                if average_arr[filename][0] in color_table['yellow'][0] and \
-                    average_arr[filename][1] in color_table['yellow'][1] and \
-                    average_arr[filename][2] in color_table['yellow'][2]:
-                    result.append(filename)
-        case 'green':
-            for filename in average_arr.keys():
-                if average_arr[filename][0] in color_table['green'][0] and \
-                    average_arr[filename][1] in color_table['green'][1] and \
-                    average_arr[filename][2] in color_table['green'][2]:
-                    result.append(filename)
-        case 'teal':
-            for filename in average_arr.keys():
-                if average_arr[filename][0] in color_table['teal'][0] and \
-                    average_arr[filename][1] in color_table['teal'][1] and \
-                    average_arr[filename][2] in color_table['teal'][2]:
-                    result.append(filename)
-        case 'blue':
-            for filename in average_arr.keys():
-                if average_arr[filename][0] in color_table['blue'][0] and \
-                    average_arr[filename][1] in color_table['blue'][1] and \
-                    average_arr[filename][2] in color_table['blue'][2]:
-                    result.append(filename)
-        case 'purple':
-            for filename in average_arr.keys():
-                if average_arr[filename][0] in color_table['purple'][0] and \
-                    average_arr[filename][1] in color_table['purple'][1] and \
-                    average_arr[filename][2] in color_table['purple'][2]:
-                    result.append(filename)
-        case _:
-            print("oh hellow there! \n the program was created by a guy, who doesn't know this color name, try something simpler")
-            return
+    threshold_range = [[],[],[]]
+    threshold_range[0] = range(target_colour[0]-threshold[0], target_colour[0]+threshold[0])
+    threshold_range[1] = range(target_colour[1]-threshold[1], target_colour[1]+threshold[1])
+    threshold_range[2] = range(target_colour[2]-threshold[2], target_colour[2]+threshold[2])
+    for filename in average_arr.keys():
+        if average_arr[filename][0] in threshold_range[0] and \
+            average_arr[filename][1] in threshold_range[1] and \
+            average_arr[filename][2] in threshold_range[2]:
+                    result.append(filename)    
     return result
 
-def do_this()-> list:
+def do_harder(target:list, threshold:list) -> list:
     dick = {}
     dick = fill_array(dick)
     average_colour_value_dick = mean_colour(dick)
     # print('\n'.join("{}\t{}".format(k, v) for k, v in average_colour_value_dick.items()))
-    result = hsv_processing(average_arr=average_colour_value_dick, target_colour = 'teal')
+    result = hsv_processing_harder(average_arr=average_colour_value_dick, target_colour = target, threshold=threshold)
     return result
 
-
 if __name__ == '__main__':
-    result = do_this()
+    target_image_path = '/home/kefear/Documents/maga_2_sem/group_proj/smart-paletter/sort_service/array/pink1.jpg'
+    target_image = cv2.imread(target_image_path)
+    target_image_hsv = format_hsv_image(target_image)
+    target = calculate_mean_colour(target_image_hsv)
+    # print(target)
+
+    # result = do_this('blue')
+    thresholds = {}
+    thresholds['black_or_white'] = [255, 30, 30]
+    thresholds[1] = [20, 30, 30]
+    thresholds[2] = [30,60,60]
+    thresholds[3] = [30,150,150]
+    thresholds[4] = [40,200,200]
+    thresholds[5] = [50, 205, 205]
+
+    result = do_harder(target, threshold = thresholds[2])
     print(result)
